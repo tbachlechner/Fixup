2554e0a899908a5f5b91737fd5f20f944d7342ed
tbachlechner <thomas@deepyeti.ucsd.edu>
2020-04-02 11:53:23 -0700

    update run.sh

--- cifar/run.sh
+++ cifar/run.sh
@@ -1,9 +1,3 @@
-CUDA_VISIBLE_DEVICES=0 python cifar_train.py -a rezero_resnet56 --sess benchmark_4110am --alpha 0. --progress_bar False
-CUDA_VISIBLE_DEVICES=0 python cifar_train.py -a rezero_resnet56 --sess benchmark_4110am --alpha 0. --progress_bar False
-CUDA_VISIBLE_DEVICES=0 python cifar_train.py -a rezero_resnet56 --sess benchmark_4110am --alpha 0. --progress_bar False
-CUDA_VISIBLE_DEVICES=0 python cifar_train.py -a resnet56 --sess benchmark_4110am --alpha 0. --progress_bar False
-CUDA_VISIBLE_DEVICES=0 python cifar_train.py -a resnet56 --sess benchmark_4110am --alpha 0. --progress_bar False
-CUDA_VISIBLE_DEVICES=0 python cifar_train.py -a resnet56 --sess benchmark_4110am --alpha 0. --progress_bar False
-CUDA_VISIBLE_DEVICES=0 python cifar_train.py -a fixup_resnet56 --sess benchmark_4110am --alpha 0. --progress_bar False
-CUDA_VISIBLE_DEVICES=0 python cifar_train.py -a fixup_resnet56 --sess benchmark_4110am --alpha 0. --progress_bar False
-CUDA_VISIBLE_DEVICES=0 python cifar_train.py -a fixup_resnet56 --sess benchmark_4110am --alpha 0. --progress_bar False
+CUDA_VISIBLE_DEVICES=0 python cifar_train.py -a rezero_resnet56 --sess 4211am --alpha 0. --progress_bar False
+CUDA_VISIBLE_DEVICES=0 python cifar_train.py -a resnet56 --sess 4211am --alpha 0. --progress_bar False
+CUDA_VISIBLE_DEVICES=0 python cifar_train.py -a fixup_resnet56 --sess 4211am --alpha 0. --progress_bar False
